b4c0519ada864bbbe64779ede48e2da950c73c60
noname <48761044+noname0310@users.noreply.github.com>
2020-02-21 01:40:16 +0900

    project create

---
+++ DiscordLinker-2.X/DiscordLinker-2.X/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DiscordLinker_2.X
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
